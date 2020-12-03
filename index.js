2707b4fbe169e94aa6b05f3a5f9474cbbdc51a47
André Aragão <andrearagaoart@gmail.com>
2020-12-03 12:39:05 -0300

    desafio resolvido

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ const cenario1 = [0, 0, 1, 0, 0, 1, 0];
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
 const jumpingOnClouds = c => {
+  let twoJumps = false;
+  const pulos = c.reduce((acc, nuvem, i, array) => {
+    
+    console.log(`index: ${i} - acc: ${acc}`);
+    console.log(`nuvem: ${nuvem} - prox: ${array[i + 1]}`);
+    
+    if(array[i + 1] === undefined) return acc;
 
+    if(twoJumps) {
+      twoJumps = false;
+      console.log('eu dei pulo duplo a partir da casa anterior até a proxima\n')
+      return acc;
+    }
+
+    if(array[i + 1] === 0 && array[i + 2] === 0) {
+      twoJumps = true;
+      console.log('pulei duas casas livres!\n')
+      return acc + 1;
+    } else if(array[i + 2] === 0) {
+      twoJumps = true;
+      console.log('pulei 2 casas por cima de uma casa eletrificada!\n')
+      return acc + 1;
+    } else {
+      console.log('pulei uma casa!\n')
+      return acc + 1;
+    }
+  }, 0);
+  
+  return pulos;
 }
 
 jumpingOnClouds(cenario1)
